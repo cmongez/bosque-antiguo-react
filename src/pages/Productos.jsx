55d4117c13489f4af9b9e9eaecf05276e49b5f39
pauzunigaa <paulinazuniga.a@gmail.com>
2025-11-18 01:21:38 -0300

    conectar productos del frontend con backend

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { ProductCard } from '../components/ProductCard'
+import { getProducts } from '../api/products'
 
 export const Productos = () => {
   const [productos, setProductos] = useState([])
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    fetch('/data/productos.json')
-      .then(res => res.json())
-      .then(data => {
+    const cargarProductos = async () => {
+      try {
+        const data = await getProducts()
         setProductos(data)
         localStorage.setItem("productos", JSON.stringify(data))
+      } catch (error) {
+        console.error("Error al cargar productos: ", error)
+      } finally {
         setLoading(false)
-      })
-      .catch(() => setLoading(false))
+      }
+    }
+
+    cargarProductos()
   }, [])
 
   const addToCart = codigo => {
@@ -28,16 +34,10 @@ export const Productos = () => {
       cart.push({ ...producto, cantidad: 1 })
     }
 
-    // Guardar carrito actualizado
     localStorage.setItem('carrito', JSON.stringify(cart))
-
-    //  Notificar al Navbar para que actualice el contador
     window.dispatchEvent(new Event('updateCart'))
-
-    // Feedback visual
     alert(`${producto.nombre} añadido al carrito`)
   }
-
 
   if (loading) return <h2 className="text-center py-5">Cargando productos...</h2>
 
@@ -46,7 +46,11 @@ export const Productos = () => {
       <h2 className="text-center mb-4">Nuestros productos</h2>
       <div className="row g-4">
         {productos.map(prod => (
-          <ProductCard key={prod.codigo} producto={prod} addToCart={addToCart} />
+          <ProductCard
+            key={prod.codigo}
+            producto={prod}
+            addToCart={addToCart}
+          />
         ))}
       </div>
     </section>
